f2b03a76c5262a438fe9a8aef557ef587cb390c9
Errol Caeiro <errolcaeiro07@gmail.com>
2024-04-06 12:12:57 +0530

    commit Ver

---
+++ Java/src/Programs/addRBCA.java
@@ -0,0 +1,16 @@
+package Programs;
+
+public class addRBCA {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        ADDRBCA22129 calculator = new ADDRBCA22129();
+
+        // Example usage of the sum method
+        int result = calculator.sum(10, 20);
+        System.out.println("Sum: " + result);
+        System.out.println("R/BCA-22-129");
+    }
+}
